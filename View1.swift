b281e34bcaa96c701f36ad572b33795cb95368eb
Ben Rudhart <hi@benrudhart.com>
2022-11-30 14:16:34 +0100

    Simplify View1

--- View1.swift
+++ View1.swift
@@ -5,11 +5,6 @@ struct View1: View {
     var body: some View {
         Group {
             Text("Hello World!")
-            button2
         }
-    }
-
-    var button2: some View {
-        Text("Press me now!")
     }
 }
