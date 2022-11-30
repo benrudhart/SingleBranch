64ec72a768e469c475bb218d5c596dba60b0e315
Ben Rudhart <hi@benrudhart.com>
2022-11-30 14:03:37 +0100

    Add button

--- View1.swift
+++ View1.swift
@@ -5,6 +5,11 @@ struct View1: View {
     var body: some View {
         Group {
             Text("Hello World!")
+            button
         }
+    }
+
+    var button: some View {
+        Text("press me")
     }
 }
